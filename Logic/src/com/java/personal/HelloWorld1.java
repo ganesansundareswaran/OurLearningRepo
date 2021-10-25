79428f3b43ed3ecde4d140539e896ccb51b27a8a
Kavitha <kavitha_ganesan@outlook.com>
2021-10-25 20:51:33 +0530

    Removing Branch name in sop

--- Logic/src/com/java/personal/HelloWorld1.java
+++ Logic/src/com/java/personal/HelloWorld1.java
@@ -3,7 +3,7 @@ package com.java.personal;
 public class HelloWorld1 {
 
 	public static void main() {
-		System.out.println("Hello World for new branch kvn_dev");
+		System.out.println("Hello World for new branch");
 	}
 
 }
